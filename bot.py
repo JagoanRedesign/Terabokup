0a44a347ef469eb66d651a3403445bb0b20e66a9
Year <165040954+YEAH-NEW@users.noreply.github.com>
2024-03-27 02:24:31 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,17 +5,17 @@ from pyrogram import Client, filters
 
 
 
-  bot = Client(
+  Client = Client(
         "GROUP-MEMBERS-BAN-BOT",
         bot_token=Config.BOT_TOKEN,
         api_id=Config.API_ID,
         api_hash=Config.API_HASH)
 
     print("🎊 I AM ALIVE 🎊")
-
-
+    Client.run()
+    
 @Client.on.message(filters.command(start) & filters.private)
 def comand1(Client, message):
     bot.send_message(message.chat.id, "hello i am a group admin bot")
 
-    Client.run()
+    
