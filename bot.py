0cc535ec1447b19cc898cccc18db28770d47690a
Year <165040954+YEAH-NEW@users.noreply.github.com>
2024-03-27 01:30:56 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,3 +2,16 @@
 
 from pyrogram import filters
 from pyrogram import Client
+
+
+
+
+    Client = Client(
+        "GROUP-MEMBERS-BAN-BOT",
+        bot_token=Config.BOT_TOKEN,
+        api_id=Config.API_ID,
+        api_hash=Config.API_HASH)
+
+    print("🎊 I AM ALIVE 🎊")
+
+    Client.run()
