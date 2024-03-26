b960f7a17d86e8f4b33ab9ed20ae5a21cb94c16f
Year <165040954+YEAH-NEW@users.noreply.github.com>
2024-03-27 02:41:30 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,11 @@
 #
 
 
-from pyrogram import Client as Ntbots
-from pyrogram import filters
+from pyrogram import Client, filters
 from config import Config
 
 
-  Ntbots=Ntbots(
+  bot = Client(
        "GROUP-MEMBERS-BAN-BOT",
         bot_token=Config.BOT_TOKEN,
         api_id=Config.API_ID,
@@ -14,11 +13,11 @@ from config import Config
 
     
     
-@Ntbots.on.message(filters.command(start) & filters.private)
-def comand1(Ntbots, message):
+@bot.on.message(filters.command(start) & filters.private)
+def comand1(bot, message):
     bot.send_message(message.chat.id, "hello i am a group admin bot")
 
 
     print("🎊 I AM ALIVE 🎊")
-    Ntbots.run()
+    bot.run()
     
