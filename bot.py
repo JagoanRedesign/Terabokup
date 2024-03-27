0b5e9885d5534f04d3a6e4feb5513da28478dba0
Year <165040954+YEAH-NEW@users.noreply.github.com>
2024-03-27 17:18:43 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, ForceRepl
 API_ID = "4888076"
 API_HASH = "8b9b8214d84305d5ba8042c93575ea84"
 BOT_TOKEN = "6101898707:AAEJnCfLSVcYWmju-bNrJRjHhm-UhzK03DI"
-GROUP = "hellllllllllllllllllllddkdk"
+#GROUP = "hellllllllllllllllllllddkdk"
 WELCOME_MESSAGE = "𝐖𝐞𝐥𝐜𝐨𝐦𝐞 𝐓𝐨 𝐍𝐓 𝐁𝐨𝐭𝐬 𝐒𝐮𝐩𝐩𝐨𝐫𝐭 𝐆𝐫𝐨𝐮𝐩"
 
 
@@ -54,7 +54,7 @@ async def help(Client, message):
 
 
 
-@bot.on_message(filters.chat(GROUP) & filters.new_chat_members)
+@bot.on_message(filters.chat_group & filters.new_chat_members)
 def welcome(Client, messgae):
 	message.reply_text(WELCOME_MESSAGE)
 
