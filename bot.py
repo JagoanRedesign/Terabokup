23713a23cf726d724e8fae3227174f47af00bab5
Year <165040954+YEAH-NEW@users.noreply.github.com>
2024-03-27 13:37:37 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -22,7 +22,7 @@ async def start(Client, message):
 
 @bot.on_message(filters.private & filters.command(["help"]))
 async def help(Client, message):
-    await message.reply_text("**I AM NOT A FULLY GROUP ADMIN BOT**\n\n**Admin commands:** 👇🏻\n /ban **Ban a user.**\n /unban **Unban a user.**\n /mute **Mute a user.**\n /unmute **Unmute a user.**\n")
+    await message.reply_text("**I AM NOT A FULLY GROUP ADMIN BOT**\n\n**Admin commands:** 👇🏻\n• /ban **Ban a user.**\n• /unban **Unban a user.**\n• /mute **Mute a user.**\n• /unmute **Unmute a user.**\n")
 
 
 
