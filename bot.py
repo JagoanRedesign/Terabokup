2c5c2b4955e710b46e1b77193a6e4e58cddbba0b
Year <165040954+YEAH-NEW@users.noreply.github.com>
2024-03-27 12:37:35 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,29 +2,9 @@
 
 
 
-
-
-
 from pyrogram import Client, filters
 
-if __name__ == "__main__" :
 
-    bot = Client(
-        "MembersBan_Bot",
-        api_id = 4888076,
-        api_hash = "8b9b8214d84305d5ba8042c93575ea84",
-        bot_token = "6986631333:AAHJ1THDOYeWasJfJ58ARCmlyGcyCB2GPO8"
-    )
-
-        
-
-@bot.on_message(filters.private & filters.command(["start"]))
-async def start(bot, message):
-    await message.reply_text(
-       caption=f"""hello im admin bot"""
-    )
-
-       print("🎊 I AM ALIVE 🎊")
-       bot.run()
-     
-    
+API_ID = ""
+API_HASH = ""
+BOT_TOKEN = ""
