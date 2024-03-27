3f7d1152b1373770f41916d0bd4a2ad41a2e18a0
Year <165040954+YEAH-NEW@users.noreply.github.com>
2024-03-27 12:58:27 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,9 +3,9 @@
 from pyrogram import Client, filters
 
 
-API_ID = ""
-API_HASH = ""
-BOT_TOKEN = ""
+API_ID = "4888076"
+API_HASH = "8b9b8214d84305d5ba8042c93575ea84"
+BOT_TOKEN = "6986631333:AAHJ1THDOYeWasJfJ58ARCmlyGcyCB2GPO8"
 
 bot = Client(
     "Admin bot",
