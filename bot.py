b0678e74a8b8b32a8df0ab84c7df43dcf414ab1d
Year <165040954+YEAH-NEW@users.noreply.github.com>
2024-03-27 12:45:45 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,3 +8,15 @@ from pyrogram import Client, filters
 API_ID = ""
 API_HASH = ""
 BOT_TOKEN = ""
+
+bot = Client(
+    "Admin bot",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN
+)
+
+
+print("🎊 I AM ALIVE 🎊")
+
+bot.run()
